9c932b8ffbe820b643953bacbefe34e7fa16d2db
andrewbastian <andrew.bastian@gmail.com>
2019-08-21 18:16:42 -0700

    working on stretch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { render } from "react-dom";
 import "./App.css";
 import BottomRow from "./BottomRow";
@@ -9,6 +9,18 @@ function App() {
 
 const [countHome, homeUpdate] = useState(0);
 const [countAway, awayUpdate] = useState(0);
+const [yardCount, gains] = useState(0);
+const [downCount, downUpdate ] = useState(0);
+useEffect(()=>{
+  function handleSatusChange(status){
+    downUpdate(status.downCount)
+  }
+})
+if(downCount === 4){
+  console.log(downCount - 4)
+}
+return downCount
+
   return (
     <div className="container">
       <section className="scoreboard">
@@ -26,7 +38,24 @@ const [countAway, awayUpdate] = useState(0);
             <div className="away__score">{countAway}</div>
           </div>
         </div>
-        <BottomRow />
+        <div className="bottomRow">
+          <div className="down">
+            <h3 className="down__title">Down</h3>
+            <div className="down__value">{downCount}</div>
+          </div>
+          <div className="toGo">
+            <h3 className="toGo__title">To Go</h3>
+            <div className="toGo__value">7</div>
+          </div>
+          <div className="ballOn">
+            <h3 className="ballOn__title">Ball on</h3>
+            <div className="ballOn__value">{yardCount}</div>
+          </div>
+          <div className="quarter">
+            <h3 className="quarter__title">Quarter</h3>
+            <div className="quarter__value">4</div>
+          </div>
+        </div>
       </section>
       <section className="buttons">
         <div className="homeButtons">
@@ -37,6 +66,17 @@ const [countAway, awayUpdate] = useState(0);
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick = {()=> awayUpdate(countAway + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick = {()=> awayUpdate(countAway + 3)}>Away Field Goal</button>
+        </div>
+        <div className = "bottomBTNs">
+<button className="awayButtons__fieldGoal" onClick = {()=> downUpdate(downCount + 1 )}>Down</button>
+<button className="awayButtons__fieldGoal" onClick = {()=> awayUpdate(countAway + 3)}>To Go</button>
+<button className="awayButtons__fieldGoal" onClick = {()=> awayUpdate(countAway + 3)}>Ball on</button>
+<button className="awayButtons__fieldGoal" onClick = {()=> awayUpdate(countAway + 3)}>quarter</button>
+<form id ="frm" action= {()=> gains(yardCount ++)}>
+  Turn over:<br></br>
+  <input type="text" number = "?"></input>
+  <input type="submit" onClick = {()=> document.getElementById('frm').submit()} value="Submit"></input>
+</form>
         </div>
       </section>
     </div>
